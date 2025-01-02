45628f66db818f43ee3f599b9d5097850554c070
Mylowkey <eemq20@gmail.com>
2025-01-02 13:04:51 -0500

    I can't keep the order of the values for each answer as I did on top when declaring the variables, it just shows the latest version of TODO.

--- js/assignment.js
+++ js/assignment.js
@@ -9,6 +9,28 @@ const todos = [
 ];
 
 // Declare your two immutable variables
+let shiftValue = ["Do some coding"];
+let popValue = ["Rake the leaves"];
+
+// console.log(todos);
+console.log(todos.toString());
+
+console.table(todos); // output the third value using tables
+console.log(todos[2]) 
+console.log(todos[2]); //output the third value 
+
+popValue = todos.pop();
+console.log(todos);   //Removed the last value 
+
+todos.reverse(); //Reversed todos
+console.log(todos); //Output reversed todos
+
+shiftValue = todos.shift(); //Removed the first value 
+console.log(todos) // output it
+
+let todo = todos.concat(shiftValue,popValue); // Added shiftValue and popValue to the end of todos.
+console.log(todo); //output
+
 
 
 // Declaring constant variables to store references to the elements
@@ -22,11 +44,11 @@ const answerSixEl = document.getElementById("answer-six");
 const submissionBtn = document.getElementById("submission-btn");
 
 function updateAnswerOne() {
-    
+    answerOneEl.textContent = todo.toString();
 }
 
 function updateAnswerTwo() {
-
+answerTwoEl.textContent = todo[2];
 }
 
 function removeLastValue() {
@@ -58,7 +80,8 @@ function updateAnswerSix() {
 }
 
 function render() {
-
+    updateAnswerOne()
+    updateAnswerTwo()
 }
 
 submissionBtn.addEventListener("click", function () {
--- js/index.js
+++ js/index.js
@@ -1,3 +1,41 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+
+// let colores = ["verde", "purpura", "marron", "melon"];
+// colores.push("negro");
+// console.log(colores);
+
+// colores.pop();
+// console.log(colores);
+// colores.push("negro");
+// console.log(colores);
+
+// colores.unshift("rojo");
+// console.log(colores);
+  
+// ////Filter
+
+// let filColors = colores.filter(color => color.includes("a"));
+// console.log(filColors);
+
+
+// ////Concat  can I do let compNumber = numbers + numbers2  ?
+
+// let numbers = [1, 2, 3, 4];
+// console.log(numbers);
+// let number2 = [5,6,7,8,9];
+// let compNumbers = numbers.concat(number2);
+// console.log(compNumbers);
+
+
+// ////reverse
+
+// compNumbers.reverse();
+// console.log(compNumbers);
+
+// ///sort
+
+// colores.sort();
+// console.log(colores);
